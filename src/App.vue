5cf8c6ec495f095ac01192c357e10452b81f0bde
matia-gov <mohamed.atia@phila.gov>
2020-03-31 10:53:31 -0400

    add search and sort, move student meals to their own repo

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
   <div id="vue-app">
     <FreeMeals />
-    <StudentMeals />
   </div>
 </template>
 
 <script>
 import FreeMeals from './components/FreeMeals.vue'
-import StudentMeals from './components/StudentMeals.vue'
+
 
 export default {
   name: 'App',
   components: {
     FreeMeals,
-    StudentMeals
   }
 }
 </script>
 
 <style>
-/* #vue-app {
-  max-width: 70rem;
-  margin: 0 auto;
-} */
-
 thead tr td {
     width: 50%;
   }
--- src/components/FreeMeals.vue
+++ src/components/FreeMeals.vue
@@ -22,19 +22,44 @@
         <i class="fas fa-times " />
       </button>
     </div>
-    <table>
+    <div
+      v-show="loading"
+      class="mtm center"
+    >
+      <i class="fas fa-spinner fa-spin fa-3x" />
+    </div>
+    <div
+      v-show="!loading && empty"
+      class="h3 mtm center"
+    >
+      Sorry, there are no results.
+    </div>
+    <div
+      v-show="failure"
+      class="h3 mtm center"
+    >
+      Sorry, there was a problem. Please try again.
+    </div>
+    <table
+      v-show="!empty && !loading && !failure"
+    >
       <thead>
         <tr>
-          <td>Name</td>
-          <td>Address</td>
+          <th
+            class="table-sort title"
+            :class="currentSortDir"
+            @click="toggleSort()"
+          >
+            <span>Food site</span>
+          </th>
+          <th>Address (ZIP code) </th>
         </tr>
       </thead>
       <tbody>
       <tr v-for="site in filteredSites" 
           :key="site.ObjectID">
-        
           <td>{{site.Agency_Name }}</td>
-         <td>{{ site.Address }} {{ site.Zip_Code}}</td>
+         <td>{{ site.Address }} ({{ site.Zip_Code}})</td>
       </tr>
       </tbody>
     </table>
@@ -57,6 +82,10 @@ export default {
       mealSites: [],
       filteredSites: [],
       search: "",
+      loading: false,
+      failure: false,
+      empty: false,
+      currentSortDir: 'desc',
       searchOptions: {
         shouldSort: true,
         threshold: 0.1,
@@ -69,26 +98,61 @@ export default {
       },
     }
   },
+
   mounted() {
     this.fetchData()
   },
+
   watch: {
     search() {
       this.searchTable();
     },
+    filteredSites() {
+      if (!this.failure) {
+        if (this.filteredSites.length == 0) {
+          this.empty = true;
+        } else  {
+          this.empty = false;
+        }
+      }
+    }
   },
+
   methods: {
     fetchData: function() {
+      this.loading = true;
       axios.get(endpoint)
       .then((response) => {
         response.data.features.forEach((item)=> {
           this.mealSites.push(item.attributes);
           this.filteredSites.push(item.attributes)
         })
+        this.sortSites();
+        this.failure = false;
+        this.loading = false;
       })
       .catch((e) => {
         console.log(e)
+        this.loading = false;
+        this.failure = true;
       })
+    },
+
+    toggleSort: function() {
+       this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
+       this.sortSites()
+    },
+
+    sortSites:function() {
+      this.filteredSites.sort((a, b) => {
+         let modifier = -1;
+          if(this.currentSortDir === 'desc') {
+            modifier = 1;
+          }
+        var textA = a.Agency_Name.toUpperCase();
+        var textB = b.Agency_Name.toUpperCase();
+        return (textA < textB) ? ( -1 * modifier) : (textA > textB) ? (1 * modifier) : 0;
+      });
     },
 
     clearSearchBar: function() {
@@ -96,17 +160,16 @@ export default {
     },
 
     searchTable: function() {
-     
       if (this.search) {
-        //  let tempSites = [];
         this.$search(this.search, this.mealSites, this.searchOptions).then(sites => {
           this.filteredSites = sites;
         });
       } else {
-
         this.filteredSites = this.mealSites;
+        this.sortSites();
       }
     }
+
   },
 }
 </script>
@@ -123,6 +186,5 @@ export default {
       z-index: 999;
       cursor: pointer;
       color: rgba(60, 60, 60, 0.5);
-      
-    }
+}
 </style>
